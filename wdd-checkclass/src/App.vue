9421c1fc7bb525320b545f53473894c63b316b29
iizcy <32973726+iizcy@users.noreply.github.com>
2020-04-13 03:02:51 +0700

    login vue

--- wdd-checkclass/src/App.vue
+++ wdd-checkclass/src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav-bar></nav-bar>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    </div>-->
+    <!-- <router-view /> -->
+    <b-container class="bv-example-col d-flex justify-content-center">
+      <b-col  md="6">
+        <img src="../image_vue/logo.png" alt />
+        <h6>IN-Class</h6>
+        <div class="button">
+          <b-button>Sign in with Google</b-button>
+        </div>
+      </b-col>
+    </b-container>
   </div>
 </template>
 
+<script>
+import NavBar_Teacher from "@/components/NavBar_Teacher.vue";
+export default {
+  components: {
+    "nav-bar": NavBar_Teacher
+  }
+};
+</script>
+
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Quicksand", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -28,5 +49,40 @@
       color: #42b983;
     }
   }
+}
+
+img {
+  width: 70%;
+}
+
+.container {
+  margin-top: 50px;
+}
+
+h6 {
+  font-size: 2em;
+  padding-top: 5%;
+  font-weight: 800;
+  text-align: center;
+}
+
+.button {
+  padding-top: 15%;
+    padding-bottom: 15%;
+
+}
+
+.btn-secondary {
+  width: 80%;
+  border-radius: 30rem;
+  font-size: 1.2em;
+  font-weight: 600;
+  background-image: linear-gradient(90deg, #57aae7, #9af0f5);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border: none;
+}
+.col-11 {
+    flex: 0 0 91.666667%;
+    max-width: 100%;
 }
 </style>
