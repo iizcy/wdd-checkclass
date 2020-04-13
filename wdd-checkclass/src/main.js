0508e13c62f822eb3ecadafcf3e4128543e325c3
60070024 <60070024@kmitl.ac.th>
2020-04-13 14:50:53 +0700

    upJson

--- wdd-checkclass/src/main.js
+++ wdd-checkclass/src/main.js
@@ -1,4 +1,5 @@
 import "@babel/polyfill";
+import firebase from 'firebase';
 import "mutationobserver-shim";
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
@@ -7,6 +8,15 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyAXt29hYIKWqS5Q9Gh4D9nd6YP4NZitknI",
+  authDomain: "wdd-classattend.firebaseapp.com",
+  databaseURL: "https://wdd-classattend.firebaseio.com",
+  projectId: "wdd-classattend",
+  storageBucket: "wdd-classattend.appspot.com",
+  messagingSenderId: "326742755496",
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
