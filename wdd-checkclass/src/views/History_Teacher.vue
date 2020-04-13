fdcbc1892ca879b93a5cd053f81e4c171045115e
60070024 <60070024@kmitl.ac.th>
2020-04-13 21:41:27 +0700

    History_Teacher

--- wdd-checkclass/src/views/History_Teacher.vue
+++ wdd-checkclass/src/views/History_Teacher.vue
@@ -1,7 +1,30 @@
 <template>
   <div id="History_Teacher">
     <nav-bar-teacher></nav-bar-teacher>
-
+    <div class="container-History_Teacher">
+      <b-container class="bv-example-row">
+      <b-row align-h="around">
+      <b-col cols="4">
+      <b-card
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>Status</b-card-text>        
+      </b-card>
+      </b-col>
+      <b-col cols="4">
+            <b-card
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>Status</b-card-text>
+                   
+      </b-card>
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-col>
+      </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -18,33 +41,16 @@ export default {
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
 
-#ClassTeacher {
+#History_Teacher {
   font-family: "Quicksand", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color:#2c3e50;
+}
+.container-History_Teacher{
+  height: 50vh;
 }
 
-.form-group {
-  padding: 2% 0%;
-  margin-bottom: 0rem;
-}
-
-.form-control {
-  border-radius: 30rem;
-}
-
-.btn-creatClass_2{
-    margin-top: 20px;
-    background-image: linear-gradient(90deg, #57aae7, #9af0f5);
-    border: none;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    
-}
-.modal-body {
-    position: relative;
-    flex: 1 1 auto;
-    padding: 10rem;
-}
 </style>
